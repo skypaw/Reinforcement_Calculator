db44d7aabd506b450f9e9cc793e8430720b953fc
BuildTools <unconfigured@null.spigotmc.org>
2019-11-06 16:35:02 +0100

    all methods for CompressionSymmetricReinforcement

--- src/pl/pawz/zelbet/ULS/CompressionSymmetricReinforcement.java
+++ src/pl/pawz/zelbet/ULS/CompressionSymmetricReinforcement.java
@@ -6,8 +6,8 @@ public class CompressionSymmetricReinforcement {
 
     private float nEd;
     private float mEd;
-    private double eCu3;
-    private double eC3;
+    private double epsilonCu3;
+    private double epsilonC3;
     private float fCd;
     private float fYd;
     private double etaConcrete;
@@ -27,14 +27,15 @@ public class CompressionSymmetricReinforcement {
     private double eS2;
 
 
-    private CompressionSymmetricReinforcement(float nEd, float mEd, double eCu3, double eC3, float fCd, float fYd,
+
+    private CompressionSymmetricReinforcement(float nEd, float mEd, double epsilonCu3, double epsilonC3, float fCd, float fYd,
                                               double etaConcrete, double lambdaConcrete, double dDimension, float bDimension,
                                               float hDimension, float a1, float a2, int E_S, double xLim, double xMinusMinYd,
                                               double xMinYd, double x0, double xMaxYd, double eS1, double eS2) {
         this.nEd = nEd;
         this.mEd = mEd;
-        this.eCu3 = eCu3;
-        this.eC3 = eC3;
+        this.epsilonCu3 = epsilonCu3;
+        this.epsilonC3 = epsilonC3;
         this.fCd = fCd;
         this.fYd = fYd;
         this.etaConcrete = etaConcrete;
@@ -52,6 +53,7 @@ public class CompressionSymmetricReinforcement {
         this.xMaxYd = xMaxYd;
         this.eS1 = eS1;
         this.eS2 = eS2;
+
     }
 
     private double xVar() {
@@ -63,10 +65,10 @@ public class CompressionSymmetricReinforcement {
     }
 
     private double xSmallerThanXMinYd() {
-        double aVar = lambdaConcrete * (fYd - eCu3 * E_S);
-        double bVar = -2 * (fYd * dDimension - eCu3 * E_S * a2 * (1 + 0.5 * lambdaConcrete));
-        double cVar = 2 * ((nEd * (fYd * eS1 - eCu3 * E_S * eS2)) / (lambdaConcrete * etaConcrete * fCd * bDimension) - eCu3 * E_S * Math.pow(a2, 2));
-        double dVar = (2 * nEd * eCu3 * E_S * a2 * eS2) / (lambdaConcrete * etaConcrete * fCd * bDimension);
+        double aVar = lambdaConcrete * (fYd - epsilonCu3 * E_S);
+        double bVar = -2 * (fYd * dDimension - epsilonCu3 * E_S * a2 * (1 + 0.5 * lambdaConcrete));
+        double cVar = 2 * ((nEd * (fYd * eS1 - epsilonCu3 * E_S * eS2)) / (lambdaConcrete * etaConcrete * fCd * bDimension) - epsilonCu3 * E_S * Math.pow(a2, 2));
+        double dVar = (2 * nEd * epsilonCu3 * E_S * a2 * eS2) / (lambdaConcrete * etaConcrete * fCd * bDimension);
         return PolynomialSolver.solver(aVar,bVar,cVar,dVar,0);
     }
 
@@ -83,7 +85,7 @@ public class CompressionSymmetricReinforcement {
 
     private double[] xGreaterThanXMinusMinYd() {
         double x = xSmallerThanXLim();
-        double sigmaS2 = eCu3 * (x - a2) / x * E_S;
+        double sigmaS2 = epsilonCu3 * (x - a2) / x * E_S;
         return new double[]{x, sigmaS2};
     }
 
@@ -91,6 +93,52 @@ public class CompressionSymmetricReinforcement {
         double aS1 = (nEd * eS2 + etaConcrete * fCd * bDimension * lambdaConcrete * x * (0.5 * lambdaConcrete * x - a2)) / (sigmaS1 * (dDimension - a2));
         double aS2 = (nEd * eS1 - etaConcrete * fCd * bDimension * lambdaConcrete * x * (dDimension - 0.5 * lambdaConcrete * x)) / (sigmaS2 * (dDimension - a2));
         return new double[]{aS1, aS2};
+    }
+
+    private double[] xGreaterThanXLim(){
+        double aVar = lambdaConcrete*(fYd+ epsilonCu3*E_S);
+        double bVar = -2*(fYd*a2+epsilonCu3*E_S*dDimension*(1+0.5*lambdaConcrete));
+        double cVar = 2*((nEd*(fYd*eS2+epsilonCu3*E_S*eS1))/(lambdaConcrete*etaConcrete*fCd*bDimension)+epsilonCu3*E_S*Math.pow(dDimension,2));
+        double dVar = (-2*nEd)/(lambdaConcrete*etaConcrete*fCd*bDimension)*epsilonCu3*E_S*dDimension*eS1;
+        return new double[] {PolynomialSolver.solver(aVar,bVar,cVar,dVar,xLim),fYd};
+    }
+
+    private double xGreaterThanH(){
+        double aVar= lambdaConcrete*(fYd+epsilonC3*E_S);
+        double bVar= -2*(fYd*(a2+0.5*lambdaConcrete*x0)+epsilonC3*E_S*dDimension*(1+0.5*lambdaConcrete));
+        double cVar= 2*((nEd*(fYd*eS2+epsilonC3*E_S*eS1))/(lambdaConcrete*etaConcrete*fCd*bDimension)+epsilonC3*E_S*Math.pow(dDimension,2)+fYd*a2*x0);
+        double dVar= (-2*nEd)/(lambdaConcrete*etaConcrete*fCd*bDimension)*(epsilonC3*E_S*dDimension*eS1+fYd*x0*eS2);
+        return PolynomialSolver.solver(aVar,bVar,cVar,dVar,hDimension);
+    }
+
+    private double xSmallerThanH(){
+        return epsilonCu3*(dDimension-xGreaterThanXLim()[0])/xGreaterThanXLim()[0]*E_S; //sigmaS1
+    }
+
+    private double xGreaterThanHbyLambda(){
+        double f1 = nEd*eS1-etaConcrete*fCd*bDimension*hDimension*(0.5*hDimension-a1);
+        double f2 = nEd*eS2+etaConcrete*fCd*bDimension*hDimension*(0.5*hDimension-a2);
+        return (epsilonC3*E_S*dDimension*f1+fYd*x0*f2)/(epsilonC3*E_S*f1+fYd*f2);
+    }
+
+    private double xSmallerThanHbyLambda(){
+        return epsilonC3*(dDimension-xGreaterThanH())/(xGreaterThanH()-x0)*E_S;
+    }
+
+    private double xBelongsToHbyLambdaAndXYdMax(){
+        return epsilonC3*(dDimension-xGreaterThanHbyLambda())/(xGreaterThanHbyLambda()-x0)*E_S;
+    }
+
+    private double[] xDoNotBelongsToHbyLambdaAndXYdMax(){
+        double f1 = nEd*(eS1*dDimension+eS2*a2)+etaConcrete*fCd*bDimension*hDimension*0.5*((a1-a2)*(dDimension+a2)-Math.pow(dDimension-a2,2));
+        double f2 = nEd*(eS1+eS2)+etaConcrete*fCd*bDimension*hDimension*(a1-a2);
+        double x = f1/f2;
+        double sigmaS2 = epsilonC3*(x-a2)/(x-x0)*E_S;
+        return new double[]{x, sigmaS2};
+    }
+
+    private double xGreaterOrEqualHByLambda(){
+        return hDimension/lambdaConcrete; //x
     }
 
 }
