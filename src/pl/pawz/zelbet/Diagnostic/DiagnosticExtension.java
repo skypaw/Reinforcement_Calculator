fb904cab59dd9f6f407ae8b5a6f292c35761f6b0
skypaw <pawzowy@gmai.com>
2019-11-11 12:45:49 +0100

    Diagnostic algorithm done, to check

--- src/pl/pawz/zelbet/Diagnostic/DiagnosticExtension.java
+++ src/pl/pawz/zelbet/Diagnostic/DiagnosticExtension.java
@@ -1,6 +1,7 @@
 package pl.pawz.zelbet.Diagnostic;
 
 import pl.pawz.zelbet.BasicValuesPillars;
+import pl.pawz.zelbet.PolynomialSolver;
 
 public class DiagnosticExtension {
     private float nEd;
@@ -73,6 +74,55 @@ public class DiagnosticExtension {
         xVar = 1/lambdaConcrete*((eS2+a2)-Math.sqrt(Math.pow(eS2+a2,2)-(2*fYd*(aS1*eS1-aS2*eS2))/(etaConcrete*fCd*bDimension)));
     }
 
+    private void xSmallerThanXMinYd(){
+        double aVar = -2*(eS2+a2)/lambdaConcrete;
+        double bVar = 2*(aS1*fYd*eS1-aS2*epsilonCu3*E_S*eS2)/(Math.pow(lambdaConcrete,2)*etaConcrete*fCd*bDimension);
+        double cVar = 2*aS2*epsilonCu3*E_S*eS2*a2/(Math.pow(lambdaConcrete,2)*etaConcrete*fCd*bDimension);
+
+        xVar = PolynomialSolver.solver(1, aVar, bVar, cVar, xLim);
+    }
+
+    private void xSmallerThanXMinMinusYd(){
+        xVar = 1/lambdaConcrete*((eS2+a2)-Math.sqrt(Math.pow(eS2+a2,2)-(2*fYd*(aS1*eS1+aS2*eS2))/(etaConcrete*fCd*bDimension)));
+    }
+
+    private void xGreaterThanZero(){
+        sigmaS1 = Math.min(epsilonCu3*(dDimension-xVar)/xVar*E_S,fYd);
+
+        sigmaS2 = epsilonCu3 * (xVar - a2) / xVar * E_S;
+        if (sigmaS2 > fYd) {
+            sigmaS2 = fYd;
+        }
+        if (sigmaS2 < -fYd) {
+            sigmaS2 = -fYd;
+        }
+    }
+
+    private void xSmallerThanZero(){
+        xVar = 0;
+        sigmaS1 = fYd;
+        sigmaS2 = -fYd;
+    }
+
+    public double[] resultsDiagnosticExtension(){
+
+        if (xVar<=xMinYd){
+            xSmallerThanXMinYd();
+            if (xVar<xMinusMinYd){
+                xSmallerThanXMinMinusYd();
+            }
+        }
+        if (xVar>0){
+            xGreaterThanZero();
+        }else{
+            xSmallerThanZero();
+        }
 
 
+
+        double nRd = -etaConcrete*fCd*bDimension*lambdaConcrete*xVar+sigmaS1*aS1-sigmaS2*aS2;
+        double mRd = etaConcrete*fCd*bDimension*lambdaConcrete*xVar*(dDimension-0.5*lambdaConcrete*xVar)+sigmaS2*aS2*(dDimension-a2)+nEd*(0.5*hDimension-a1);
+
+        return new double[]{nRd, mRd};
+    }
 }
