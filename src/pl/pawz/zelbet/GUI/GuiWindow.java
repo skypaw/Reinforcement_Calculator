df1931d1fb782195a613b3914740bef03066a624
BuildTools <unconfigured@null.spigotmc.org>
2019-11-20 12:12:11 +0100

    Scroll pane gui

--- src/pl/pawz/zelbet/GUI/GuiWindow.java
+++ src/pl/pawz/zelbet/GUI/GuiWindow.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class GuiWindow extends Application {
@@ -17,8 +18,13 @@ public class GuiWindow extends Application {
 
         Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
 
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(root);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+
         primaryStage.setTitle("Reinforcement Calculator");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(scrollPane));
         primaryStage.show();
 
     }
