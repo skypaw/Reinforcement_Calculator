10972362daca799c609d5f63c62b95be4a0b9457
BuildTools <unconfigured@null.spigotmc.org>
2020-01-07 01:45:44 +0100

    Converting project into maven

--- src/pl/pawz/zelbet/SLS/Deflection.java
+++
@@ -1,8 +0,0 @@
-package pl.pawz.zelbet.SLS;
-
-public class Deflection {
-
-    public Deflection(float mEk, float mEkLt, float mCr, float eCm, float fCtm, float alphaM, float lEff, float epsilonCS){
-
-    }
-}
